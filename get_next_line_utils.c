241174a008484b9252b7d310693e1588e13345b1
Pornpawee Wareepornthep <pwareepo@z2t17c2.42bangkok.com>
2023-02-25 18:28:12 +0700

    add buf

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,36 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pwareepo <pwareepo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/18 14:35:57 by pwareepo          #+#    #+#             */
-/*   Updated: 2023/02/25 18:27:24 by pwareepo         ###   ########.fr       */
+/*   Created: 2023/02/25 18:24:48 by pwareepo          #+#    #+#             */
+/*   Updated: 2023/02/25 18:25:12 by pwareepo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-char	*get_next_line(int fd)
-{
-	buf = malloc (sizeof(char) * (BUFFER_SIZE + 1));
-	if (buf == NULL)
-		return (NULL);
-	read (fd, buf, BUFFER_SIZE);
-}
-
-// #include <stdio.h>
-// int	test()
-// {
-// 	static int n = 0;
-// 	n++;
-// 	return (n);
-// }
-// int main()
-// {
-// 	static int x;
-// 	static int y;
-// 	int z;
-// 	printf("%d\n%d\n%d", test(), test(), test());
-// }
